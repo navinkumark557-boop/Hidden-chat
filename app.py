01ab5cfa70164647800a9002495774030a6aca0a
navinkumark557-boop <navinkumark557@gmail.com>
2026-06-01 14:16:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask
+
+from flask import Flask, request, redirect, session, render_template_string
 
 app = Flask(__name__)
+app.secret_key = "secret123"
 
-@app.route('/')
-def home():
-    return "<h1>Private Chat App Working 🚀</h1>"
+ROOM_CODE = "PLC2026"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+LOGIN_PAGE = """
+<h2>Private Chat Room</h2>
+<form method="post">
+<input name="username" placeholder="Your Name" required><br><br>
+<input name="code" placeholder="Room Code" required><br><br>
+<button type="submit">Enter</button>
+</form>
+"""
+
+CHAT_PAGE = """
+<h2>Private Chat Room</h2>
+
+<div id="chat" style="height:300px;border:1px solid #000;overflow:auto;padding:10px;"></div>
+
+<input id="msg" placeholder="Message">
+<button onclick="sendMsg()">Send</button>
+
+<script>
+function sendMsg(){
+ let msg = document.getElementById("msg").value;
+ let box = document.getElementById("chat");
+ box.innerHTML += "<p><b>You:</b> " + msg + "</p>";
+ document.getElementById("msg").value="";
+}
+</script>
+"""
+
+@app.route("/", methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+        if request.form["code"] == ROOM_CODE:
+            session["user"] = request.form["username"]
+            return redirect("/chat")
+        return "
